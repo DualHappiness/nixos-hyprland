80f691da71dab10a6d862e7c50b3c01e13ecaf2d
dualwu <dualwu_tech@outlook.com>
2025-06-10 14:52:44 +0800

    add zellij forgot

--- home/zellij.nix
+++ home/zellij.nix
@@ -1,4 +1,8 @@
 {
+  home.file.".config/zellij/plugin/forgot.wasm".source = builtins.fetchurl {
+    url = "https://github.com/karimould/zellij-forgot/releases/download/0.4.2/zellij_forgot.wasm";
+    sha256 = "31194145519dbdc128685b456f970374378fa19fc9da742fbe4a321bace449db";
+  };
   programs.zellij = {
     enable = true;
     settings.show_startup_tips = false;
@@ -18,6 +22,14 @@
         };
         "bind \"Alt Enter\"" = {
           ToggleFocusFullscreen = [ ];
+        };
+        "bind \"Alt x\"" = {
+          CloseFocus = [ ];
+        };
+        "bind \"Ctrl y\"" = {
+          "LaunchOrFocusPlugin \"file:~/.config/zellij/plugin/forgot.wasm\"" = {
+            "floating" = true;
+          };
         };
       };
     };
